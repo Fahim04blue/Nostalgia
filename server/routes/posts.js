2d2da6cf6a5c9c7ddebf10823fad9dafed7f8350
Shahriar Fahim <shahriarfahim19@gmail.com>
2021-06-23 03:01:11 +0600

    [UPDATED]User Access

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,14 +6,15 @@ import {
   updatePosts,
   likePosts,
 } from "../controllers/postsController.js";
+import auth from "../middleware/userMiddleware.js";
 
 const router = express.Router();
 
 //localhost:5000/posts
 router.get("/", getPosts);
-router.post("/", createPosts);
-router.patch("/:id", updatePosts);
-router.delete("/:id", deletePosts);
-router.patch("/:id/likePost", likePosts);
+router.post("/", auth, createPosts);
+router.patch("/:id", auth, updatePosts);
+router.delete("/:id", auth, deletePosts);
+router.patch("/:id/likePost", auth, likePosts);
 
 export default router;
