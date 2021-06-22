091806c726816a4b4fe79815c83e7d0ac5830bc9
Shahriar Fahim <shahriarfahim19@gmail.com>
2021-06-23 03:12:17 +0600

    [UPDATED]Like Functionality

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -50,14 +50,23 @@ export const deletePosts = async (req, res) => {
 export const likePosts = async (req, res) => {
   const { id } = req.params;
 
+  if (!req.userId) return res.json({ message: "Unauthorized" });
+
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
   const post = await PostMessage.findById({ _id: id });
-  const updatedPost = await PostMessage.findByIdAndUpdate(
-    id,
-    { likeCount: post.likeCount + 1 },
-    { new: true }
-  );
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
   res.status(200).json(updatedPost);
 };
